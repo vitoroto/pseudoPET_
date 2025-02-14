5f30bdca866c2362eda5b8c881e2543df671a3d3
vromero2021 <v.romero.2021@alumnos.urjc.es>
2025-02-14 15:14:40 +0100

    dcm2nii hecho

--- 001_Sort/Dcm2Nifti.py
+++ 001_Sort/Dcm2Nifti.py
@@ -1,14 +1,13 @@
 import os
 import shutil
-import dicom2nifti
 
-os.chdir('/home/vromero2021/Escritorio/TFG/002-DATA')
+
+os.chdir('/home/vromero2021/NAS/TFGs/2021_VictorRomero/002-DATA')
 
 for patient in os.listdir('.//DCM_ORIGINAL/DICOM_with_FDG_NAC/sourcedata'):
     for modality in os.listdir(f'.//DCM_ORIGINAL/DICOM_with_FDG_NAC/sourcedata/{patient}'):
-        print(f'Processing {modality} for patient {patient}\n')
+        print(f'Processing {modality} for patient {patient}...')
         if not os.path.exists(f'.//NIFTI/NIFTI_with_FDG_NAC/{patient}/{modality}'):
             os.makedirs(f'.//NIFTI/NIFTI_with_FDG_NAC/{patient}/{modality}')
-            dicom2nifti.convert_dir.convert_directory(f'./DCM_ORIGINAL/DICOM_with_FDG_NAC/sourcedata/{patient}/{modality}',
-            f'./NIFTI/NIFTI_with_FDG_NAC/{patient}/{modality}', compression=True, reorient=True)
+            os.system(f'dcm2niix -o /home/vromero2021/NAS/TFGs/2021_VictorRomero/002-DATA/NIFTI/NIFTI_with_FDG_NAC/{patient}/{modality} -z y -b y /home/vromero2021/NAS/TFGs/2021_VictorRomero/002-DATA/DCM_ORIGINAL/DICOM_with_FDG_NAC/sourcedata/{patient}/{modality}')
             print(f'Done {modality} for patient {patient}\n')
