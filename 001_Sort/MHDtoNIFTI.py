a33addb696169f2b4e5db18e9b938c8e87dff923
vromero2021 <v.romero.2021@alumnos.urjc.es>
2025-02-19 15:41:07 +0100

    Registration

--- 001_Sort/MHDtoNIFTI.py
+++ 001_Sort/MHDtoNIFTI.py
@@ -6,9 +6,4 @@ def convert_mhd_to_nifti(input_path, output_path):
     image = sitk.ReadImage(input_path)
 
     # Write as NIfTI
-    sitk.WriteImage(image, output_path)
-
-
-# Example usage
-convert_mhd_to_nifti("/home/vromero2021/Escritorio/r/result.mhd", "/home/vromero2021/Escritorio/r/result.nii.gz")
-
+    sitk.WriteImage(image, output_path)# Example u
--- 001_Sort/ttrial.py
+++ 001_Sort/ttrial.py
@@ -5,26 +5,79 @@ import cv2
 import numpy as np
 import SimpleITK as sitk
 from MHDtoNIFTI import convert_mhd_to_nifti
+import shutil
+
+def create_dir(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
 pathSlicer = '/home/vromero2021/Descargas/Slicer-5.8.0-linux-amd64/Slicer'
-
 pathExtension = '/home/vromero2021/Descargas/Slicer-5.8.0-linux-amd64/slicer.org/Extensions-33216/SlicerElastix/lib/Slicer-5.8/elastix'
-fixed = '/home/vromero2021/Escritorio/TFG/002-DATA/NIFTI/NIFTI_with_FDG_NAC/sub-0018/CT/CT_PETCT_Brain_LM_FDG_STAT_BDD_142747_2.nii.gz'
-moving = '/home/vromero2021/Escritorio/TFG/002-DATA/NIFTI/NIFTI_with_FDG_NAC/sub-0018/FLAIR/FLAIR_T2_FLAIR3D_SAG_1.2X1.2X1.2_152032_4.nii.gz'
+temp_intermideate = '/home/vromero2021/Escritorio/m'
 rigid_parameters = '/home/vromero2021/Descargas/Slicer-5.8.0-linux-amd64/slicer.org/Extensions-33216/SlicerElastix/lib/Slicer-5.8/qt-scripted-modules/Resources/RegistrationParameters/Parameters_Rigid.txt'
-out_path = '/home/vromero2021/Escritorio/m'
+temporal_result = '/home/vromero2021/Escritorio/r'
+pathExtension_transformix = '/home/vromero2021/Descargas/Slicer-5.8.0-linux-amd64/slicer.org/Extensions-33216/SlicerElastix/lib/Slicer-5.8/transformix'
 
-os.system(f'{pathSlicer} --launch {pathExtension} -f {fixed} -m {moving} -p {rigid_parameters} -out {out_path}')
 
 
-out_out = '/home/vromero2021/Escritorio/r'
-pathExtension = '/home/vromero2021/Descargas/Slicer-5.8.0-linux-amd64/slicer.org/Extensions-33216/SlicerElastix/lib/Slicer-5.8/transformix'
 
-os.system(f"{pathSlicer} --launch {pathExtension} -tp '/home/vromero2021/Escritorio/m/TransformParameters.0.txt' -out {out_out} -in {moving}")
+input = '/home/vromero2021/Escritorio/TFG/002-DATA/NIFTI/NIFTI_with_FDG_NAC'
+output = '/home/vromero2021/Escritorio/TFG/002-DATA/NIFTI_REGISTERED/NIFTI_with_FDG_NAC'
+for patient in os.listdir(input):
+    create_dir(os.path.join(output, patient))
+    for modality in os.listdir(os.path.join(input, patient)):
+        if modality == 'CT':
+            create_dir(os.path.join(output, patient, modality))
+            for volume in os.listdir(os.path.join(input, patient, modality)):
+                if volume.endswith('.nii.gz'):
+                    fixed_CT = os.path.join(input, patient, modality, volume) # /home/vromero2021/Escritorio/TFG/002-DATA/NIFTI/NIFTI_with_FDG_NAC/sub-0001/CT/CT_PETCT_Brain_LM_FDG_STAT_BDD_130108_2.nii.gz
+                    shutil.copy(fixed_CT, os.path.join(output, patient, modality, volume))
 
-for mhd in os.listdir(out_path):
-    if mhd.endswith('.mhd'):
-        result_mhd = os.path.join(out_path, mhd)
+        if modality == 'FDG':
+            create_dir(os.path.join(output, patient, modality))
+            for volume in os.listdir(os.path.join(input, patient, modality)):
+                if volume.endswith('.nii.gz'):
+                    fixed_FDG = os.path.join(input, patient, modality, volume) # /home/vromero2021/Escritorio/TFG/002-DATA/NIFTI/NIFTI_with_FDG_NAC/sub-0001/CT/CT_PETCT_Brain_LM_FDG_STAT_BDD_130108_2.nii.gz
+                    shutil.copy(fixed_FDG, os.path.join(output, patient, modality, volume))
 
-convert_mhd_to_nifti(result_mhd, '/home/vromero2021/Escritorio/result.nii.gz')
+        elif modality == 'FDG_NAC':
+            create_dir(os.path.join(output, patient, modality))
+            for volume in os.listdir(os.path.join(input, patient, modality)):
+                if volume.endswith('.nii.gz'):
+                    fixed_FDG_NAC = os.path.join(input, patient, modality, volume) # /home/vromero2021/Escritorio/TFG/002-DATA/NIFTI/NIFTI_with_FDG_NAC/sub-0001/CT/CT_PETCT_Brain_LM_FDG_STAT_BDD_130108_2.nii.gz
+                    shutil.copy(fixed_FDG_NAC, os.path.join(output, patient, modality, volume))
+
+        elif modality == 'FLAIR':
+            create_dir(os.path.join(output, patient, modality))
+            for volume in os.listdir(os.path.join(input, patient, modality)):
+                if volume.endswith('.nii.gz'):
+                    moving_FLAIR = os.path.join(input, patient, modality, volume)  # /home/vromero2021/Escritorio/TFG/002-DATA/NIFTI/NIFTI_with_FDG_NAC/sub-0001/CT/CT_PETCT_Brain_LM_FDG_STAT_BDD_130108_2.nii.gz
+                    os.system(
+                        f'{pathSlicer} --launch {pathExtension} -f {fixed_CT} -m {moving_FLAIR} -p {rigid_parameters} -out {temp_intermideate}')
+                    os.system(
+                        f"{pathSlicer} --launch {pathExtension_transformix} -tp '{temp_intermideate}/TransformParameters.0.txt' -out {temporal_result} -in {moving_FLAIR}")
+                    for mhd in os.listdir(temporal_result):
+                        if mhd.endswith('.mhd'):
+                            result_mhd = os.path.join(temporal_result, mhd)
+                        convert_mhd_to_nifti(result_mhd, f'/home/vromero2021/Escritorio/TFG/002-DATA/NIFTI_REGISTERED/NIFTI_with_FDG_NAC/{patient}/{modality}/Registered_FLAIR.nii.gz')
+                    os.system(f'rm -rf {temp_intermideate}/*')
+                    os.system(f'rm -rf {temporal_result}/*')
+                    print('saved')
+        elif modality == 'T1':
+            create_dir(os.path.join(output, patient, modality))
+            for volume in os.listdir(os.path.join(input, patient, modality)):
+                if volume.endswith('.nii.gz'):
+                    moving_T1 = os.path.join(input, patient, modality, volume)  # /home/vromero2021/Escritorio/TFG/002-DATA/NIFTI/NIFTI_with_FDG_NAC/sub-0001/CT/CT_PETCT_Brain_LM_FDG_STAT_BDD_130108_2.nii.gz
+                    os.system(
+                        f'{pathSlicer} --launch {pathExtension} -f {fixed_CT} -m {moving_T1} -p {rigid_parameters} -out {temp_intermideate}')
+                    os.system(
+                        f"{pathSlicer} --launch {pathExtension_transformix} -tp '{temp_intermideate}/TransformParameters.0.txt' -out {temporal_result} -in {moving_T1}")
+                    for mhd in os.listdir(temporal_result):
+                        if mhd.endswith('.mhd'):
+                            result_mhd = os.path.join(temporal_result, mhd)
+                    convert_mhd_to_nifti(result_mhd, f'/home/vromero2021/Escritorio/TFG/002-DATA/NIFTI_REGISTERED/NIFTI_with_FDG_NAC/{patient}/{modality}/Registered_T1.nii.gz')
+                    os.system(f'rm -rf {temp_intermideate}/*')
+                    os.system(f'rm -rf {temporal_result}/*')
+
+
 
